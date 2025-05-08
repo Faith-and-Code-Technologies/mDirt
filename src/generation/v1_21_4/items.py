5b903135d8bb36c8f7a99b01f8c98d4a52a054ab
TheJupiterDev <thatwierdodude@gmail.com>
2025-05-08 13:48:56 -0500

    Began Jinja rewrite for Items

---
+++ src/generation/v1_21_4/items.py
@@ -0,0 +1,11 @@
+import ast, os, shutil
+from jinja2 import Environment, FileSystemLoader
+
+class ItemGenerator:
+    def __init__(self):
+        pass
+
+
+class ItemResourcer:
+    def __init__(self):
+        pass
