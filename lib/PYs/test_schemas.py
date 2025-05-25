b996cc349eff53a3c713fe29dcc3fee98a73a88c
TheJupiterDev <thatwierdodude@gmail.com>
2025-05-25 15:23:26 -0500

    JsonToQt -> KoreUI

--- lib/PYs/test_schemas.py
+++ lib/PYs/test_schemas.py
@@ -1,25 +1,17 @@
 import sys
 import json
 from PySide6.QtWidgets import QApplication, QPushButton, QLayout
-from jsontoqt import JsonForm, load_json_schema
+from koreui import load_schema, JsonSchemaForm
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    schema = load_json_schema("src/generation/v1_21_4/schemas/enchantment.json")
+    schema = load_schema("src/generation/v1_21_4/schemas/enchantment.json")
 
     with open('lib/1.21.4_data.json', 'r') as f:
         data = json.load(f)
 
-    builder = JsonForm(schema, data)
-    form = builder.build_form()
+    form = JsonSchemaForm(schema, "Enchantment")
     form.show()
-
-    def on_submit():
-        print(json.dumps(builder.get_form_data(), indent=2))
-
-    submit_btn = QPushButton("Submit")
-    submit_btn.clicked.connect(on_submit)
-    form.layout().addWidget(submit_btn)
 
     sys.exit(app.exec())
