90132d4a1a56e88bf099fd6e0937570106b97ef4
TheJupiterDev <thatwierdodude@gmail.com>
2025-06-09 15:32:51 -0500

    🐛 Fixed Recipes with a custom Horse Armor as their output being invalid

--- src/generation/v1_21_4/recipes.py
+++ src/generation/v1_21_4/recipes.py
@@ -26,7 +26,6 @@ class RecipeGenerator:
                 if self.recipes[recipe]["exact"]:
                     recip = self.recipes[recipe]["items"]
                     letters = {"0": "A", "1": "B", "2": "C", "3": "D", "4": "E", "5": "F", "6": "G", "7": "H", "8": "I"}
-
                     content = self.getTemplate('shaped.json.j2', {
                         'ingredients': recip,
                         'outputCount': self.recipes[recipe]['outputCount'],
