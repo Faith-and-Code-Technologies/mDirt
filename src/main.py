9a8ff0ea9a56fb039a0b64b737da75bacd035835
TheJupiterDev <thatwierdodude@gmail.com>
2025-06-10 09:39:09 -0500

    🔖 Bump version numbers

--- src/main.py
+++ src/main.py
@@ -27,7 +27,7 @@ from settings import SettingsManager
 from module import ModuleDownloader
 
 APP_VERSION = '3.0.0'
-FULL_APP_VERSION = '3.0.0-pre.1'
+FULL_APP_VERSION = '3.0.0-pre.2'
 LIB_URL = 'https://raw.githubusercontent.com/Faith-and-Code-Technologies/mDirt/main/lib'
 ISSUE_URL = 'https://github.com/Faith-and-Code-Technologies/mDirt/issues'
 
