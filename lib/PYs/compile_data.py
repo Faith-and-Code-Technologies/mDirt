e01bfca41a4894116a6663e8ac87d873091557d8
Jupiter Dev <141870656+TheJupiterDev@users.noreply.github.com>
2025-09-30 12:29:28 -0500

    mc1.21.9

--- lib/PYs/compile_data.py
+++ lib/PYs/compile_data.py
@@ -286,6 +286,6 @@ def get_minecraft_files(version: str, soundver: str):
 # 1.21.4 = 19
 # 1.21.5 = 24
 # 1.21.6/1.21.7/1.21.8 = 26 <- this file is NOT the same for 1.21.6 and 7 versions though!
-# 25w31a/32a/33a/34a = 27
+# 1.21.9 = 27
 # We have NO IDEA why Mojang names their sounds this way.
-get_minecraft_files("1.21.9-pre2", "27")
+get_minecraft_files("1.21.9", "27")
